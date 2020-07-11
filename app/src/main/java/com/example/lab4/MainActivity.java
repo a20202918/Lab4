a650748510c0da74d0182db28e272bc653da245c
NatMuHe98 <a20202943@pucp.edu.pe>
2020-07-11 10:10:39 -0500

    Inicio de sesion con Facebook

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,14 +1,86 @@
 package com.example.lab4;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 12;
+    List<AuthUI.IdpConfig> providers;
+    Button btn_sign_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_sign_out = (Button)findViewById(R.id.button_CerrarSesion);
+        btn_sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>(){
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                btn_sign_out.setEnabled(false);
+                                iniciarSesion();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(),  Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        providers= Arrays.asList(
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+
+        iniciarSesion();
+    }
+
+    private void iniciarSesion() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),MY_REQUEST_CODE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "" +user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                btn_sign_out.setEnabled(true);
+            }else{
+                Toast.makeText(this, "" +response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
